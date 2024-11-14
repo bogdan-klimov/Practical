ef1dae94994776d6838123c719a4ae4c0e33a821
Bohdan Klimov <bodyaklimov333@gmail.com>
2024-11-14 21:56:25 +0100

    loop 1-5 added

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// Some changes 
+Console.WriteLine("1-5:");
+
+for (int i =0; i < 5;i++) {
+    Console.WriteLine(i+1);
+}
